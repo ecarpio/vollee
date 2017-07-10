7a3ccfe80147c69141dd3b79ec5c0a98c236ec26
Angela Carpio <angelacarpio@Ed-Carpio.local>
2017-07-10 14:41:08 -0700

    updates

--- js/function.js
+++ js/function.js
@@ -1,3 +1,17 @@
 $(function(){
 	console.log('test')
+function fixHeight(){
+	var elementHeights = $('.testimonials .col p').map(function() {
+    	return $(this).height();
+  	}).get();
+
+	var maxHeight = Math.max.apply(null, elementHeights);
+
+	 $('.testimonials .col').height(maxHeight + 50); }
+
+	fixHeight();
+
+	$( window ).resize(function() { 
+		fixHeight();
+	})
 })
